ee5733a834dc62ce8fadfb3e2f55a0adb4d4a434
Максим Сергеевич <krovorgen@gmail.com>
2020-12-21 14:51:15 +0300

    Project configured

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: 'index_bundle.js',
-		environment: {
-			arrowFunction: false, // <-----
-		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-		],
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		port: 9000,
-	},
-	plugins: [new HtmlWebpackPlugin()],
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+        environment: {
+            arrowFunction: false, // <-----
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [new HtmlWebpackPlugin()],
 }
