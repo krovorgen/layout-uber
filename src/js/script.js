bc88aee61b1eb1449644bcfb3a9e4e0811ab9d81
Максим Сергеевич <krovorgen@gmail.com>
2020-12-22 22:53:42 +0300

    edited js file

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu');
-        const menuItem = document.querySelectorAll('.menu_item');
-        const hamburger = document.querySelector('.hamburger')
+    const menu = document.querySelector('.menu')
+    const menuItem = document.querySelectorAll('.menu_item')
+    const hamburger = document.querySelector('.hamburger')
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active')
